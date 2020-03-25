1f58dadea15f6f6ef6d9572e1e98c46c512c69b9
Ametx <proffmade@yandex.ru>
2020-03-25 14:40:10 +0300

    First commit

---
+++ a.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# sudo chmod u+x a.sh
+# sudo cp a.sh /usr/local/bin/
+# ls -lart
+# .bashrc
+# export set PATH=$PATH:~/myscript
+# alias script.sh='sh script.sh'
+
+read -p "install: " cmd
+sudo apt install $cmd
